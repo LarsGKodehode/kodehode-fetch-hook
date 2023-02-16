a3ba9e8a57fc6380f6e08cb335925ae626d3a8c8
LarsG <lars.ga.solheim@gmail.com>
2023-02-16 11:12:22 +0100

    finish simple example of using a custom hook
    conditional rendering for fetch in progress, errors and finaly displaying the data

--- src/components/FetchData/FetchData.jsx
+++ src/components/FetchData/FetchData.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react"
+import useFetch from "../../hooks/useFetch"
+
 function FetchData() {
+  const [postNumber, setPostNumber] = useState(1)
+  const { isLoading, error, data } = useFetch("https://dummyjson.com/posts/" + postNumber)
+
+  function nextPost() {
+    setPostNumber(
+      (oldPost) => oldPost + 1
+    )
+  }
+
   return (
     <div>
       <h4>Fetch Data Component</h4>
+      <button onClick={nextPost}>Next Post</button>
+
+      <article>
+        {
+          isLoading
+            ? <h5>Loading post</h5>
+            : error
+              ? <h5>Error</h5>
+              :
+              <>
+                <h5>{data.title}</h5>
+                <p>{data.body}</p>
+              </>
+        }
+      </article>
+
     </div>
   )
 }
